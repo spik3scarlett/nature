c5163c2cd49bc5fcc7c5cb78e68c39b886078c54
Spik3Scarlett <gitarsitar@gmail.com>
2018-03-15 14:02:12 +0100

    wersja 2.0

--- app/controllers/health_assessment_controller.rb
+++ app/controllers/health_assessment_controller.rb
@@ -1,5 +1,5 @@
 class HealthAssessmentController < ApplicationController
-
+protect_from_forgery with: :exception
     def create
        $i = 0
        $record = params["zdrowie"]
@@ -23,6 +23,14 @@ class HealthAssessmentController < ApplicationController
     end
 
     def pytania4
+      redirect_to ankieta_path if $record == nil || $record.empty?
+    end
+
+    def pytania5
+     redirect_to ankieta_path if $record == nil || $record.empty?
+    end
+
+    def pytania6
      redirect_to ankieta_path if $record == nil || $record.empty?
     end
 
@@ -31,15 +39,23 @@ class HealthAssessmentController < ApplicationController
       if $i == 1
         $record.reverse_merge!(params["zdrowie"])
         redirect_to "/ankieta3"
-      else
+      elsif $i == 2
         $record.reverse_merge!(params["zdrowie"])
         redirect_to "/ankieta4"
+      elsif $i == 3
+        $record.reverse_merge!(params["zdrowie"])
+        redirect_to "/ankieta5"
+      else
+        $record.reverse_merge!(params["zdrowie"])
+        redirect_to "/ankieta6"
       end
     end
 
     def wynik
-      redirect_to ankieta_path if $record == nil || $record.empty? || $i != 2
-      $parametry = {pokarmowy: 10, oczyszczanie: 10, sklad_ciala: 10, ruchu: 10, gruczolowy: 10, moczowy: 10, oddechowy: 10, immune: 10, nerwowy: 10, kardio: 10, watroba: 10 }
+      redirect_to ankieta_path if $record == nil || $record.empty? || $i != 4
+      $record.reverse_merge!(params["zdrowie"])
+      $parametry = {pokarmowy: 10, oczyszczanie: 10, energia: 10, ruchu: 10, gruczolowy: 10, moczowy: 10, oddechowy: 10, immune: 10, nerwowy: 10, kardio: 10, watroba: 10 }
+    
     end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   get '/ankieta', to: 'health_assessment#pytania'
   post '/ankieta', to: 'health_assessment#create'
   get '/ankieta2', to: 'health_assessment#pytania2'
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
   get '/ankieta3', to: 'health_assessment#pytania3'
   post '/ankieta3', to: 'health_assessment#update'
   get '/ankieta4', to: 'health_assessment#pytania4'
-  post '/ankieta4', to: 'health_assessment#wynik'
+  post '/ankieta4', to: 'health_assessment#update'
+  get '/ankieta5', to: 'health_assessment#pytania5'
+  post '/ankieta5', to: 'health_assessment#update'
+  get '/ankieta6', to: 'health_assessment#pytania6'
+  post '/ankieta6', to: 'health_assessment#wynik'
   get '/wynik', to: 'health_assessment#wynik'
 
 end
